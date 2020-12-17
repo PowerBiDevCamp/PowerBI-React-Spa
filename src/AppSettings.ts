f0b4319dd4420905a11a9ec40df514fb6ea8461b
Ted Pattison <ted@tedpattison.net>
2020-12-17 11:06:00 -0500

    Updates

---
+++ src/AppSettings.ts
@@ -0,0 +1,5 @@
+export default class AppSettings {
+  static aadTenant: string = "YOUR_DOMAIN.onMicrosoft.com";
+  static clientId: string = "ADD_CLIENT_ID_FOR_APP_WITH_IMPLICIT_FLOW";
+  static appWorkspaceId: string = "ADD_APP_WORKSPACE_ID";
+}
